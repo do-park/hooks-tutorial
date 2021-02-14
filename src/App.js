f205f62cb96c4b04a52e1f2dbf9a0c243b728feb
박도희 <dohee.pa@gmail.com>
2021-02-14 23:10:53 +0900

    8.2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import Info from './Info';
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <>
       <Counter />
-      <Info />
+      <button onClick={() => {
+        setVisible(!visible);
+      }}>
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
     </>
   );
 };
